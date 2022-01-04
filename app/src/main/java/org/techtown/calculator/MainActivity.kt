a065aecc81c0846073b8308ad61f14afa14e304a
chaep <qpfl92929@naver.com>
2022-01-04 21:11:45 +0900

    day1

---
+++ app/src/main/java/org/techtown/calculator/MainActivity.kt
@@ -0,0 +1,30 @@
+package org.techtown.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
+
+    }
+
+    fun historyButtonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
+
+    }
+
+
+}
